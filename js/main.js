449e7206f1ed40f9a4c734e3085228fcfd269c84
mellisdesigns <mitch@mellisdesigns.com>
2015-05-06 01:35:08 +0800

    - Fixed some minor spelling mistakes in the main document
    Signed-off-by: mellisdesigns <mitch@mellisdesigns.com>

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 $(document).ready(function () {
 
     /*
-        Some quick specific page based jquery events to demonstrate input sub categories.
-        This is also an example of how I like to write Javascript.
+        Some quick  page based jquery events to demonstrate input sub categories. This is also an example of how I like to write Javascript.
+        
+        For this quick demonstration I added the StringPasser into the main.js file, but I like to separate these utility type helpers into their own files. Of course it's kind of different when using an MVC based framework.
         
         and a cat . . .
         
@@ -23,13 +24,11 @@ $(document).ready(function () {
     //Listener for Example 3 Input Sub using jQuery
     $('input[id=product_weight_eg3]').keyup(function(){
         
-        console.log("Key Event Called");
-        
         //Check to see whether we can parse string
         var getUnitType = StringPasser.ReturnUnitFromInput($(this).val());
         
         //If our string passer does not return null, change the label, else we show na
-        //When using the model in Ember or Angular, this will be different.
+        //When using the model in Ember or Angular, we will use data binding to update labels
         if(getUnitType) $('p[data-id=label_eg3_unit]').text(getUnitType);
         else $('p[data-id=label_eg3_unit]').text('NA');
         
@@ -47,10 +46,10 @@ $(document).ready(function () {
             if(splitInput.length == 2)
             {
                 //Next we should check whether the first value is a number
-                //We do this by trying to cast the first value of the array toa number, if it is not, we will get NaN.
+                //We do this by trying to cast the first value of the array to a number, if it is not, we will get NaN.
                 if(!isNaN(Number(splitInput[0])))
                 {
-                    //We can consider this a succesfull pass of the string
+                    //We can consider this a successful pass of the string
                     //Return the unit from the string passed in.
                     return splitInput[1];
                 }
